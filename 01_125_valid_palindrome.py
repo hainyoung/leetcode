39251e9b851458329474a112ec2f23b4f11fbfcc
young2e <pnp3737@gmail.com>
2020-09-03 21:38:37 +0900

    01&02

--- 01_125_valid_palindrome.py
+++ 01_125_valid_palindrome.py
@@ -1,46 +1,117 @@
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        strs = []
-        for c in s:
-             if c.isalnum():
-               strs.append(c.lower())
-        # print(strs)
-        
-        while len(strs)>1:
-            if strs.pop(0) != strs.pop(): # pop(0) : 맨 앞, pop() : 맨 끝
-                return False
-        return True
+# 01. Valid Palindrome
+# 주어진 문자열이 팰린드롬인지 확인하라, 대소문자 구분 X, 영문자와 숫자만을 대상으로 한다
 
+# input : "A man, a plan, a canal: Panama"
+# output : true
 
-# deque를 활용
+# input : "race a car"
+# output : false
 
-from collections import deque
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        strs = deque()
-        for c in s:
-            if c.isalnum():
-                strs.append(c.lower())
-        while len(strs) > 1:
-            if strs.popleft() != strs.pop():
-                return False
-        return True
+# sol1) list 
 
-
+# step1) 문자열 입력 받아 알맞게 변환
 '''
 s = input()
 strs = []
-
 for char in s:
-    if char.isalnum(): # 영어와 숫자만을 대상으로, isalnum() 함수를 사용한다
-        strs.append(char.lower()) # strs라는 빈 리스트에 받은 문장의 요소들을 넣어두는데 이 때, 영어는 소문자로 모두 바꿔준다
+    if char.isalnum():
+        strs.append(char.lower())
 
 print(strs)
+'''
+# isalnum: alphabet, number 판별 함수, 해당하는 문자만 추가 
+# 대소문자 구분 않으므로 lower()로 모두 소문자로 변환
+# 입력값이 A man, a plan, a canal: Panama 일 때 
+# strs 리스트는 
+# ['a', 'm', 'a', 'n', 'a', 'p', 'l', 'a', 'n', 'a', 'c', 'a', 'n', 'a', 'l', 'p', 'a', 'n', 'a', 'm', 'a']'
 
-while len(strs) > 1:  # strs의 길이가 하나가 되면 while문 escape
-    if strs.popleft() != strs.pop(): # pop을 사용하면 하나씩 뽑아내고 없어짐, 이 성질을 이용해서 왼쪽, 오른쪽 끝 요소를 비교하고 
-                                    # 다르면 False, 한 개 요소가 남을 때까지 모두 같으면 True를 반환
-        return False
+# step2) 팰린드롬 여부 판별
+'''
+while len(strs) > 1 :
+    if strs.pop(0) != strs.pop():
+        print(False)
+'''
+# 파이썬의 리스트는 pop() 함수에서 인덱스를 지정할 수 있기 때문에
+# 이처럼 0을 지정하면 맨 앞의 값을 가져올 수 있다
+# 이렇게 맨 뒷부분의 pop() 결과와 매칭해 나가면서 일치하지 않는 경우 False를 출력한다
+# 모두 통과했다면 True를 출력
+
+# 함수로 정의
+from dataclasses import dataclass
+@dataclass
+class Solution_1:
+    def isPalindrome_1(self, s: str): # bool
+        strs = []
+        for char in s:
+            if char.isalnum():
+                strs.append(char.lower())
+
+        # 팰린드롬 여부 판별
+        while len(strs) > 1:
+            if strs.pop(0) != strs.pop():
+                return False
+
+        return True
+
+s_1 = 'race a car'
+test_1 = Solution_1()
+print(test_1.isPalindrome_1(s_1)) # False
+
+# sol2) dque
+# 리스트만으로도 충분히 문제를 해결할 수 있지만
+# deque, 데크를 명시적으로 선언하면 좀 더 속도를 높일 수 있다
+
+from collections import deque
+def isPalindrome_2(s: str):
+    # 자료형 deqeu로 선언
+    strs: Deque = deque()
+
+    for char in s:
+        if char.isalnum():
+            strs.apppend(char.lower())
+
+    while len(strs) > 1:
+        if strs.popleft() != strs.pop():
+            return False
     return True
 
-'''
+# 자료형을 데크로 선언하는 것만으로 시간이 단축
+# 리스트 풀이 대비 거의 5배 가까이 속도 높임
+# 리스트의 pop(0)은 O(n), 데크의 popleft()는 O(1)이기 때문에
+# 각각 n번씩 반복하면 리스트 구현은 O(n^2), 데크 구현은 O(n)으로 성능차이가 큼
+
+
+# sol3) Slicing
+import re
+def isPalindrome_3(s: str):
+    s = s.lower()
+    # 정규식으로 불필요한 문자 필터링
+    s = re.sub('[^a-z0-9]', '', s)
+
+    return s == s[::-1] # 슬라이싱 
+
+#정규식으로 불필요한 문자를 필터링하고 문자열 조작하는 파이썬의 슬라이싱 사용
+# 앞선 풀이에서는 isalnum()으로 모든 문자를 일일이 점검했다면
+# 여기서는 문자열 전체를 한 번에 영숫자(Alphanumeric)만 걸러내도록 정규식으로 처리!
+# 또한 파이썬은 문자열을 배열이나 리스트처럼 자유롭게 슬라이싱할 수 있는 좋은 기능을 제공하며
+# [::-1]을 이용하면 뒤집을 수 있다
+# 코드가 훨씬 더 줆어듦은 물론, 내ㅐ부적으로 C로 빠르게 구현되어 있어 훨씬 더 좋은 속도를 기대할 수 있다
+# sol2에 비해 약 2배 정도 빠른 속도
+
+
+# 문자열 슬라시이
+# 파이썬에서는 문자열 슬라이싱이라는 매우 편리한 기능을 제공
+# 무엇보다 내부적으로 매우 빠르게 동작
+# 위치를 지정하면 해당 위치의 배열 포인터를 얻게 되며
+# 이를 통해 연결된 객체를 찾아 실제 값을 찾아내는데, 이 과정은 매우 빠르게 진행되므로
+# 문자열을 조작할 때는 항상 슬라이싱을 우선으로 사용하는 편이 속도 개선에 유리
+# 문자열을 별도로 리스트로 매핑하는 등의 처리는 데이터 구조를 다루는 입장에선 좋은 방법이지만
+# 별도 자료형으로 매핑하는 과정에서 상당한 연산 비용이 필요
+# 전체적인 속도에서는 오히려 손해
+# 대부분의 문자열 작업은 슬라이싱으로 처리하는 편이 가장 빠르다
+
+sentence = '안녕하세요'
+tmp = sentence[1:4]
+
+print(sentence)
+print(tmp)
--- 02_344_reverse_string.py
+++ 02_344_reverse_string.py
@@ -1,21 +1,58 @@
+# 02 Reverse String
+
+# 문자열을 뒤집는 함수를 작성하라, 입력값은 문자 배열, 리턴 없이 리스트 내부를 조작!
+# input : ["h", "e", "l", "l", "o"]
+# output : ["o", "l", "l", "e", "h"]
+
+# sol1) Two Pointer, Swap
+# Two Pointer를 이용한 전통적인 풀이방식
+# 투 포인터 간단히 설명?
+# 단어 그대로 2개의 포인터를 이용해 범위를 조정해가며 풀이하는 방식
+# 여기서는 점점 더 범위를 좁혀가며 swap 하는 형태로 풀이할 수 있음
+# 문제에서 '리턴 없이 리스트 내부를 직접 조작하라'는 제약 사항이 있으므로
+# 당므과 같이 s 내부를 스왑 하는 형태로 풀이하면 된다
 from typing import List
 
-# class Solution:
-#     def reverseString(self, s: List[str]) -> None:
-#         """
-#         Do not return anything, modify s in-place instead.
-#         """
-#         s.reverse()
+def reverseString_1(s:List[str]):
+    left, right = 0, len(s) - 1
+    while left < right:
+        s[left], s[right] = s[right], s[left]
+        left += 1
+        right -= 1
 
 
-# class Solution:
-def reverseString(self, s: List[str]) -> None:
 
-    p, q = 0, len(s) - 1 
-    while p < q :
-        s[p], s[q] = s[q], s[p]
-        p += 1
-        q -= 1
+# sol2) pythonic way
+# 파이썬의 기본 기능 이용하면 단 한 줄로 가능
+# 이러한 방식을 흔히 파이썬다운 방식 "Pythonic Way"라고 한다
+# 입력값이 리스트로 제공되므로 reverse() 함수를 사용하면 뒤집을 수 있다
+
+def reverseString_2(s: List[str]):
+    s.reverse()
 
 
-        
+s = ["h", "e", "l", "l", "o"]
+s.reverse()
+
+print(s) # ['o', 'l', 'l', 'e', 'h']
+
+# reverse()는 리스트에만 제공된다
+# 만약 입력값이 문자열이라면 앞서 살펴본 바와 같이
+# 문자열 슬라이싱을 사용할 수 있다
+# 슬라이싱은 리스트에도 사용할 수 있으며 성능 또한 매우 좋다
+
+
+# s = s[::-1]
+# 그러나 이 풀이는 리트코드에서는 오류가 발생
+# 원래는 [::-1]도 정상적으로 처리되어야 하지만
+# 이 문제의 경우 처리되지 않는데,
+# 이 문제는 공간 복잡도를 O(1)로 제한하다보니
+# 변수 할당을 처리하는 데 다소 제약이 있다
+# 이 때 다음과 같은 트릭을 사용하면 잘 동작한다
+
+# s[:] = s[::-1]
+# 이런 트릭은 쉽게 알아내기 어려움
+# 실제 코딩 테스트 시에도 이 같은 문제가 발생하면
+# 디버깅에 애를 먹을 수 있으므로, 플랫폼의 특징에 대해 충분한 숙지가 필요
+
+# 
