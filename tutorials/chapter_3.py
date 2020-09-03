eb348cc3991a33e03563585fa9963d6e388f5c0f
young2e <pnp3737@gmail.com>
2020-09-03 20:42:29 +0900

    0903

--- tutorials/chapter_3.py
+++ tutorials/chapter_3.py
@@ -60,6 +60,8 @@ print(a) # [2, 6, 10, 14, 18]
 # 중간값을 리턴한 다음 함수는 종료되지 않고 계속해서 맨 끝에 도달할 때까지 실행된다
 # 물론 아래의 코드처럼 while Ture 구문은 종료 조건이 없으므로 계속해서 값을 내보낼 수 있다
 
+
+'''
 def get_natural_number():
     n = 0
     while True:
@@ -77,6 +79,8 @@ print(get_natural_number())
 g = get_natural_number()
 for _ in range(0, 100):
     print(next(g))
+'''
+
 
 # 제너레이터는 다음과 같이 여러 타입의 값을 하나의 함수에서 생성하는 것도 가능
 def generator():
@@ -117,6 +121,7 @@ print()
 # 생성 조건만 정해두고 나중에 필요할 때 생성해서 꺼내 쓸 수 있다
 # 다음은 숫자 100만개를 생성하는 2가지 방법
 
+'''
 # (1)
 a = [n for n in range(1000000)]
 b = range(1000000)
@@ -149,6 +154,8 @@ print("b의 방식 :", sys.getsizeof(b)) # 48
 # 다음과 같이 리스트와 거의 동일한 느낌으로 불편 없이 사용할 수 있다
  
 print(b[999]) # 999
+'''
+
 
 # enumerate
 # enumerate()는 '열거하다'는 뜻의 함수로, 순서가 있는 자료형(list, set, tuple 등)을 인덱스를 포함한
@@ -206,4 +213,244 @@ print(test) # (1, 2)
 
 # print
 # 코딩 테스트 문제 풀이 과정에서 디버깅을 할 때 가장 자주 쓰는 명령 : print()
+# 대부분 코딩 테스트 플랫폼에서는 적어도 print()를 통해 stdout 출력결과를 부여주어 디버깅 용도로 활용할 수 있게 지원함
+# 하지만 정답 제출시에는 print() 조차 보여주지 않는 경우가 있으니 유의!
+# 실무에서는 이처럼 print()를 활용하는 디버깅 방법은 추천하지 않는다
+# 하지만 코딩 테스트 시에는 디버거를 사용하거나 TDD방식으로 접근하기도 어렵기 때문에,
+# 사실상 print()가 디버깅을 위해 제공되는 유일한 기능
+# 이를 좀 더 유용하게 활용할 수 있는 방법?
+# 가장 쉽게 값을 출력하는 방법은 콤마 , 로 구분하는 것
+# 이 경우 한 칸 공백이디폴트로 설정되어 있으며, 그대로 출력하면 띄어쓰기로 값을 구분해준다.
+
+print('A1', 'B2') # A1 B2
+
+# sep 파라미터로 구분자를 콤마 , 로 지정해 줄 수도 있다
+print('A1', 'B2', sep=',') # A1,B2
+
+# print() 함수는 항상 줄바꿈을 하기 때문에 긴 루프의 값을 반복적으로 출력하면 디버깅 하기가 어려운데
+# 이 경우 다음과 같이 end 파라미터를 공백으로 처리하여 줄바꿈을 하지 않도록 제한할 수 있다
+
+print('aa', end=' ')
+print('bb')
+# aa bb
+
+# 리스트를 출력할 때는 join()으로 묶어서 처리
+aaa = ['A', 'B']
+print(' '.join(aaa)) # A B
+
+# 다음과 같이 idx와 fruit이 정의되어 있을 때
+idx = 1
+fruit = 'Apple'
+# idx 값에 1을 더해서 fruit와 함께 출력하는 방법?
+print('{0}: {1}'.format(idx+1, fruit)) # 2: Apple
+
+# index 생략도 가능
+print('{}: {}'.format(idx+1, fruit)) # 2: Apple
+
+# f-string
+print(f'{idx+1}: {fruit}') # 2: Apple
+# 기존의 %를 사용하거나 .format을 부여하는 방식에 비해 훨씬 간결, 직관적, 속도도 빠름
+# f-string은 3.6+에서만 지원!
+
+# pass
+# 코딩을 하다 보면 일단 코드의 전체 골격을 잡아 놓고 
+# 내부에서 처리할 내용은 차근차근 생각하며 만들겠다는 의도로 다음과 같이 코딩하는 경우도 있다
+
 #
+from dataclasses import dataclass
+# @dataclasses
+# class MyClass(object):
+#     def method_a(self):
+
+#     def method_b(self):
+#         print('Method B')
+
+# c =MyClass()
+
+# 위를 실행하면 아래의 오류 발생
+'''
+    def method_b(self):
+          ^
+IndentationError: expected an indented block
+'''
+# 이 문제는 method_a()가 아무런 처리를 하지 않았기 때문에 엉뚱하게 method_b()에서 오류가 발생한 것
+# 필요한 오류이긴 하지만 한참 개발을 하던 중에 이런 오류를 맞닥뜨리게 되면 생각보다 처리하기가 번거롭다
+# pass는 이런 오류를 막는 역할을 한다
+# 다음과 같이 pass를 method_a()에 삽입해 간단히 처리할 수 있다
+
+@dataclass
+class MyClass(object):
+    def method_a(self):
+        # 여기에 pass 추가
+        pass
+    def method_b(self):
+        print('Method B')
+
+c = MyClass()
+print(c.method_b())
+'''
+Method B
+None
+'''
+
+# 파이썬에서 pass는 Null Operation 널 연산으로 아무것도 하지 않는 기능이 있다
+# 이처럼 아무 역할을 하지 않는 pass를 지정하면 앞서 발생한 인덴트 오류 같은 불필요한 오류를 방지할 수 있다
+# 이렇게 pass는 먼저 mockup 인터페이스부터 구현한 다음 추후 구현을 진행할 수 있게 한다
+# 온라인 코딩 테스트에서 아주 유용
+
+# try 
+# except : Exception
+# tracebak.print_exc()
+
+
+# locals
+# locals()는 로컬 심볼 테이블 딕셔너리를 가져오는 메소드로 업데이트 또한 가능하다
+# 여기서는 딕셔너리를 가져오는 부분에 집중해 살펴보자면,
+# 우선 로컬에 선언된 모든 변수를 조회할 수 있는 강력한 명령이므로 디버깅에 많은 도움이 된다
+# 특히 로컬 스코프에 제한해 정보를 조회할 수 있기 때문에, 클래스의 특정 메소드 내부에서나 함수 내부의 로컬 정보를 조회해
+# 잘못 선언한 부분이 없는지 확인하는 용도로 활용할 수 있다
+# 변수명을 일일이 찾아낼 필요 없이 로컬 스코프에 정의된 모든 변수를 출력하기 때문에 편리하다
+# 리트코드 문제 풀이 중에도 코드 내부에 출력해 활용할 수 있다
+
+import pprint
+pprint.pprint(locals())
+
+'''
+{'MyClass': <class '__main__.MyClass'>,
+ '__annotations__': {},
+ '__builtins__': <module 'builtins' (built-in)>,
+ '__cached__': None,
+ '__doc__': None,
+ '__file__': 'd:\\leetcode\\tutorials\\chapter_3.py',
+ '__loader__': <_frozen_importlib_external.SourceFileLoader object at 0x00000165EA4CEB08>,
+ '__name__': '__main__',
+ '__package__': None,
+ '__spec__': None,
+ 'aaa': ['A', 'B'],
+ 'c': MyClass(),
+ 'dataclass': <function dataclass at 0x00000165EC403828>,
+ 'enu': [1, 2, 3, 2, 45, 2, 5],
+ 'fruit': 'Apple',
+ 'gen': <generator object generator at 0x00000165EA4F2A48>,
+ 'generator': <function generator at 0x00000165EA55D798>,
+ 'i': 2,
+ 'idx': 1,
+ 'pprint': <module 'pprint' from 'C:\\Users\\inyoung\\anaconda3\\lib\\pprint.py'>,
+ 'test': (1, 2),
+ 'v': 'c3'}
+'''
+
+# 파이썬 코딩스타일
+
+# - snake case
+# - 변수명도 의미를 담아서
+# - 간단한 주석 부여(영어로 달아서 제출하면 더 프로페셔널 ㅋㅋ)
+# 최소한 자연스럽게 영어로 주석을 읽고 쓰는 데 아무런 부담이 없을 정도로 익숙해져야 한다
+
+# - 리스트 컴프리헨션
+# 역할별로 줄을 구분하면 가독성 높아진다
+import re
+
+str1 = ['helloworld']
+str1s = [
+    str1[i:i + 2].lower() for i in range(len(str1) - 1)
+    if re.findall('[a-z]{2}', str1[i:i + 2].lower())
+]
+
+# 풀어 쓰면
+str1s = []
+for i in range(len(str1) - 1):
+    if re.finidall('[a-z]{2}', str1[i:i + 2].lower()):
+        str1s.append(str1[i:i + 2].lower())
+
+# 가독성을 위해서는 위도 나쁘지 않다
+# 코드를 읽는 방향이 위에서 아래까지 차례대로 정방향이기 때문에 훨씬 이해하기 쉽다
+# 짧게 쓰는 것을 고집할 필요 없음
+# 리스트 컴프리헨션은 대체로 표현식이 1개를 넘지 않도록!
+
+# Google's Python Style Guide
+# 함수의 기본 값으로 가변 객체(Mutable Object)를 사용하지 않아야 한다
+# 함수가 객체를 수정하면(리스트에 아이템을 추가한다든지 등) 기본값이 변경되기 때문
+# 따라서 다음과 같이 기본값으로 []나 {}를 사용하는 것은 지양!
+
+# Bad Case
+# def foo(a, b=[]):
+# def foo(a, b:Mapping={})
+
+# Good Case : 불변객체(Immutable Object) 사용
+# None을 명시적으로 할당!
+def foo_1(a, b=None):
+    if b is None:
+        b = []
+
+
+# def foo_2(a, b:Optional[Sequence] = None):
+    # if b is None:
+        # b = []
+
+# True, False를 판별할 때는 암시적(Implicit)인 방법을 사용하는 편이 간결, 가독성 높음
+# 즉, 굳이 False임을 if foo !=[]: 같은 형태로 판별할 필요가 없다
+# if foo:로 충분하다
+
+# Good Case
+
+# (1)
+# users = []
+
+# if not users:
+#     print('no users')
+
+# (2)
+# foo = 0
+# if foo == 0:
+#     self.handle_zero()
+
+# (3)
+# i = 10
+
+# if i % 10 == 0:
+#    self.handle_mutiple_of_ten()
+
+# # Bad Case
+# if len(users) == 0:
+#     print('no users')
+
+# if foo is not None and not foo:
+#     self.handle_zero()
+
+# if not i % 10:
+#     self.handle_multiple_of_ten()
+
+# len(users) == 0 은 길이가 없다는 말 == 값이 없다는 뜻
+# (1)과 같이 not users로 충분다
+
+# 정수를 처리할 때는 암시적으로 거짓 여부를 판별하기보다 (2)와 같이 비교 대상이 되는 정수값을 직접 비교하는 편이 덜 위험
+
+# (3)과 같이 i % 10 == 0 으로 명시적으로 explicitly 값을 비교하는 편이 좋다
+
+# 최대 줄 길이는 80자로 제한(암묵적 약속)
+
+import this
+'''
+The Zen of Python, by Tim Peters
+
+Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+Although never is often better than *right* now.
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!
+'''
