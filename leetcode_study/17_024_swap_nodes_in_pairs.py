3d88a1f484ce6ee29e09b277a501127d79170564
young2e <pnp3737@gmail.com>
2020-09-18 16:32:13 +0900

    0918

--- leetcode_study/17_024_swap_nodes_in_pairs.py
+++ leetcode_study/17_024_swap_nodes_in_pairs.py
@@ -28,6 +28,24 @@ class Solution1:
             cur = cur.next.next
 
         return head
+# explanation
+# 직관적인 방법 동시에 변칙적인 방법
+# 연결리스트의 노드를 변경하는 게 아닌 노드 구조는 그대로 유지하되 값만 변경
+# 실용성과는 거리가 먼 방법
+# 대개 연결 리스트는 복잡한 여러 가지 값들의 구조체로 구성되어 있고
+# 사실상 값만 바꾸는 게 어려운 일
+# 그러나 이 문제에서는 단 하나의 값으로 구성된 단순한 연결리스트이고 값을 바꾸는 정도는 어렵지 않다
+# list = 1 - 2 - 3 - 4
+# cur = 1
+# while cur and cur.next: -> 1, 2
+# cur.val = cur.next.val -> 1 = 2
+# cur.next.val = cur.val -> 2 = 1
+# 2 -> 1
+# cur = cur.next.next -> cur = 1.next.next == 3
+# 3 = 4, 4 = 2
+# 4 -> 3
+
+# 2 - 1 - 4 - 3
 
 # 2. swap using repetitive structure
 
@@ -54,6 +72,11 @@ class Solution2:
             prev = prev.next.next
         
         return root.next
+
+
+
+
+
 
 # 3. swap using recursive structure
 @dataclass
